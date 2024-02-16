fa380ce765c26f54c97f6cc67161631679ff973d
EunJiKU8151 <yoo128@easymedia.net>
2024-02-16 14:01:49 +0900

    정리

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const methods = useForm();
 
   return (
-    // FormProvider로 컴포넌트를 감싸주면 어디서든 react-hook-form에 등록된 input의 값에 접근 가능!!!
+    // FormProvider: 컴포넌트를 감싸주면 어디서든 react-hook-form에 등록된 input의 값에 접근 가능!!!
     <FormProvider {...methods}>
       <Header />
       <Table />
